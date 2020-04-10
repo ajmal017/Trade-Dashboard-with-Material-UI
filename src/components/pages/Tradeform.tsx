b24e02be627eb2bb0ade324c70c44700b20b00bf
jessicali61 <jessicalearning61@gmail.com>
2020-04-10 16:57:58 -0400

    the trade form

--- src/components/pages/Tradeform.tsx
+++ src/components/pages/Tradeform.tsx
@@ -1,73 +1,127 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Typography,
-  BottomNavigation,
-  BottomNavigationAction,
+  AppBar,
+  Tabs,
+  Tab,
   FormControl,
   InputLabel,
   Select,
   MenuItem,
-  Input,
-  Button
-} from '@material-ui/core';
+  TextField,
+  Button,
+} from "@material-ui/core";
 
 export class Tradeform extends Component {
   state = {
     action: "buy",
-    orderType: "market-order"
-  }
+    orderType: "market-order",
+    orderSize: 100,
+    price: 0,
+  };
 
   changeAction = (event: any, value: string) => {
-    // console.log(event);
-    // console.log(value);
+    console.log(event);
+    console.log(value);
     this.setState({
-      action: value
+      action: value,
     });
-  }
+  };
 
-  changeOrderType = (event: any) => {
-    // console.log(event.target.value);
+  onFormChange = (event: any) => {
+    // console.log(event);
     this.setState({
-      orderType: event.target.value
-    })
-  }
+      [event.target.name]: event.target.value,
+    });
+  };
 
   render() {
     return (
       <div>
-        <Typography variant="h5">Stock Name</Typography>
-        <BottomNavigation
-          showLabels
-          value={this.state.action}
-          onChange={this.changeAction}
-        >
-          <BottomNavigationAction label="Buy" value="buy"></BottomNavigationAction>
-          <BottomNavigationAction label="Sell" value="sell"></BottomNavigationAction>
-        </BottomNavigation>
-        <form>
-          <FormControl style={{ width: "45%", minWidth: 120, marginRight: "10%" }}>
-            <InputLabel id="order-type-label">Order Type</InputLabel>
+        <Typography variant="h5" style={{ marginBottom: "0.8rem" }}>
+          Stock Name
+        </Typography>
+        <AppBar position="static">
+          <Tabs
+            centered
+            variant="fullWidth"
+            indicatorColor="primary"
+            value={this.state.action}
+            onChange={this.changeAction}
+            style={{ background: "#3d5170", color: "#fff" }}
+          >
+            <Tab label="BUY" value="buy" />
+            <Tab label="SELL" value="sell" />
+          </Tabs>
+        </AppBar>
+        <form style={{ marginTop: "1.5rem" }}>
+          <FormControl
+            variant="outlined"
+            style={{
+              width: "48%",
+              minWidth: 120,
+              marginRight: "4%",
+              marginBottom: "0.8rem",
+            }}
+          >
+            <InputLabel id="order-type-label" shrink>
+              Order Type
+            </InputLabel>
             <Select
               labelId="order-type-label"
               id="order-type"
+              name="orderType"
               value={this.state.orderType}
-              onChange={this.changeOrderType}
+              onChange={this.onFormChange}
+              label="Order Type"
             >
               <MenuItem value="market-order">Market Order</MenuItem>
               <MenuItem value="limit-order">Limit Order</MenuItem>
-              <MenuItem value="trailing-stop-order">Trailing Stop Order</MenuItem>
+              <MenuItem value="trailing-stop-order">
+                Trailing Stop Order
+              </MenuItem>
               <MenuItem value="stop-order">Stop Order</MenuItem>
               <MenuItem value="stop-limit-order">Stop Limit Order</MenuItem>
             </Select>
           </FormControl>
-          <FormControl style={{ width: "45%", minWidth: 120 }}>
-            <InputLabel htmlFor="order-size">Order Size</InputLabel>
-            <Input id="order-size" />
+          <FormControl
+            style={{ width: "48%", minWidth: 120, marginBottom: "0.8rem" }}
+          >
+            {/* <InputLabel htmlFor="order-size" shrink>Order Size</InputLabel> */}
+            <TextField
+              id="order-size"
+              label="Order Size"
+              variant="outlined"
+              name="orderSize"
+              value={this.state.orderSize}
+              onChange={this.onFormChange}
+            />
           </FormControl>
+          <FormControl
+            style={{ width: "48%", minWidth: 120, marginRight: "4%" }}
+          >
+            {/* <InputLabel htmlFor="order-price" shrink>Price</InputLabel> */}
+            <TextField
+              id="order-price"
+              label="Price"
+              variant="outlined"
+              name="price"
+              value={this.state.price}
+              onChange={this.onFormChange}
+            />
+          </FormControl>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            style={{ width: "48%", minWidth: 120, color: "#fff" }}
+          >
+            PLACE ORDER
+          </Button>
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default Tradeform
+export default Tradeform;
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,14 +1,17 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { cyan, lightGreen } from '@material-ui/core/colors';
+import { cyan, green } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
   palette: {
+    type: "dark",
     primary: {
       main: "#399fdb",
       light: cyan.A200,
       dark: "#3f4d63",
     },
-    secondary: lightGreen,
+    secondary: {
+      main: green.A400,
+    },
     text: {
       primary: "#399fdb",
       secondary: "#fff"
